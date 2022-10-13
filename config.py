861f58c48f810bc2f3e25dde45121dc9d8abbcc2
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-10-13 22:11:41 +0100

    Update config.py

--- config.py
+++ config.py
@@ -8,4 +8,11 @@ class Config:
 
     def __init__(self):
 
+        # the data directory
         self.data_directory = os.path.join(os.getcwd(), 'data')
+
+        # the number film recommendations to present to a client
+        self.slate_size = 5
+
+        # instead of updating an online algorithm after each event, update it after every <batch_event> events
+        self.batch_size = 10
