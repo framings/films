69137c96d9a0faa6e74c5193e24fd00496b9ca61
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-10-13 23:36:53 +0100

    Update src/data/preprocessing.py

--- src/data/preprocessing.py
+++ src/data/preprocessing.py
@@ -11,7 +11,7 @@ class Preprocessing:
 
         """
 
-        logging.basicConfig(level=logging.INFO, format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
+        logging.basicConfig(level=logging.WARNING, format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
                             datefmt='%Y-%m-%d %H:%M:%S')
         self.logger = logging.getLogger(__name__)
 
