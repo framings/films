86b281f870d24b406ff385fd7ea5d02768df9224
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-10-13 15:57:34 +0100

    Update src/data/preprocessing.py

--- src/data/preprocessing.py
+++ src/data/preprocessing.py
@@ -14,8 +14,25 @@ class Preprocessing:
                             datefmt='%Y-%m-%d %H:%M:%S')
         self.logger = logging.getLogger(__name__)
 
-    def exc(self, frame: pd.DataFrame):
+    def __summary(self, frame: pd.DataFrame):
 
-        summary = frame['movieId'].value_counts()
-        self.logger.info(type(summary))
+        summary = frame['movieId'].value_counts().rename('frequency').to_frame()
+        summary.reset_index(drop=False, inplace=True)
+        summary.rename(columns={'index': 'movieId'}, inplace=True)
         self.logger.info(summary)
+
+        return summary
+
+    def exc(self, frame: pd.DataFrame, limit: int):
+
+        # the number of ratings per film
+        summary = self.__summary(frame=frame)
+        self.logger.info(summary.shape)
+
+        # focus on films that have at list <limit> number of ratings
+        summary = summary.copy().loc[summary['frequency'] >= limit, :]
+        self.logger.info(summary.shape)
+
+        # hence
+        data = frame.copy().loc[frame['movieId'].isin(summary['movieId']), :]
+        self.logger.info(f'data: {data.shape}')
