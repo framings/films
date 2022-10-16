50679d9f854d62484bf161a4d38dc15fe63ec339
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-10-16 16:19:44 +0100

    Update src/algorithms/bayesianucb.py

--- src/algorithms/bayesianucb.py
+++ src/algorithms/bayesianucb.py
@@ -35,7 +35,10 @@ class BayesianUCB:
 
     def score(self, history: pd.DataFrame, boundary: int):
         """
-
+        A reference for scores['ucb'] formula: https://www.itl.nist.gov/div898/handbook/prc/section1/prc14.htm
+        
+        :param history:
+        :param boundary:
         :return:
         """
 
