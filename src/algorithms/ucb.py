f81366b5b15f20c40a84c39ff64184916c739a90
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-10-16 22:46:14 +0100

    Update src/algorithms/ucb.py

--- src/algorithms/ucb.py
+++ src/algorithms/ucb.py
@@ -96,6 +96,6 @@ class UCB:
 
         # metrics
         cumulative = np.cumsum(rewards, dtype='float64')
-        running = pd.Series(cumulative).rolling(window=self.average_window).mean().iloc[self.average_window:].values
+        running = pd.Series(rewards).rolling(window=self.average_window).mean().iloc[self.average_window:].values
 
         return self.Rewards(rewards=rewards, cumulative=cumulative, running=running)
