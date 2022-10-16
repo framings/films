a120ce39cb91b78f09625c984d304b9378eda8d5
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-10-16 21:24:14 +0100

    Update src/algorithms/random.py

--- src/algorithms/random.py
+++ src/algorithms/random.py
@@ -83,7 +83,7 @@ class Random:
         for index in range((self.data.shape[0] // self.batch_size)):
 
             # temporary break point
-            if index > 99999:
+            if index > 9999:
                 break
 
             # hence
@@ -92,9 +92,6 @@ class Random:
             if action_score is not None:
                 values = action_score['liked'].tolist()
                 rewards.extend(values)
-
-        # history
-        self.logger.info(f'History:\n {history}')
 
         # metrics
         cumulative = np.cumsum(rewards, dtype='float64')
