3a412044be64a9ba0083b71bd9e8550e0d7375f7
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-10-16 22:45:59 +0100

    Update src/algorithms/epsilongreedy.py

--- src/algorithms/epsilongreedy.py
+++ src/algorithms/epsilongreedy.py
@@ -110,6 +110,6 @@ class EpsilonGreedy:
 
         # metrics
         cumulative = np.cumsum(rewards, dtype='float64')
-        running = pd.Series(cumulative).rolling(window=self.average_window).mean().iloc[self.average_window:].values
+        running = pd.Series(rewards).rolling(window=self.average_window).mean().iloc[self.average_window:].values
 
         return self.Rewards(rewards=rewards, cumulative=cumulative, running=running)
