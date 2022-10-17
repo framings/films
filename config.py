820a9d1c02501110203d44c7f93ba824ae7ca605
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-10-17 16:19:42 +0100

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ class Config:
         # running average window
         self.average_window = 200
 
-        # 95% confidence interval parameters w.r.t. two-tailed-test
+        # Note, for a two-tailed-test 95% confidence interval:
         # alpha = 1 - 0.95
         # self.critical_value = scipy.stats.norm.ppf(q=(1 - alpha/2))
         self.critical_value = 1.5
@@ -32,21 +32,22 @@ class Config:
         slate_size: the number film recommendations to present to a client
         batch_size: instead of updating an online algorithm after each event, update it after every <batch_event> events
         average_window: the length of the running average window
-        critical_value: the critical value of the two-tailed-test confidence interval of interest
 
         :return:
         """
 
-        # Note, for a two-tailed-test 95% confidence interval:
-        # alpha = 1 - 0.95
-        # critical_value = scipy.stats.norm.ppf(q=(1 - alpha/2))
-
         Arguments = collections.namedtuple(typename='Arguments',
-                                           field_names=['slate_size', 'batch_size', 'average_window', 'critical_value'])
-        return Arguments(slate_size=5, batch_size=100, average_window=200, critical_value=1.5)
+                                           field_names=['slate_size', 'batch_size', 'average_window'])
+        return Arguments(slate_size=5, batch_size=100, average_window=200)
 
     @staticmethod
     def hyperparameters():
+        """
+        slate_size: the number film recommendations to present to a client
+        batch_size: instead of updating an online algorithm after each event, update it after every <batch_event> events
+
+        :return:
+        """
 
         Arguments = collections.namedtuple(typename='Arguments', field_names=['slate_size', 'batch_size'])
 
