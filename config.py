b1e5068cdb1a991f2a5a4b84c265c2ae7e808c19
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-10-17 15:27:59 +0100

    Update config.py

--- config.py
+++ config.py
@@ -28,11 +28,23 @@ class Config:
 
     @staticmethod
     def models():
+        """
+        slate_size: the number film recommendations to present to a client
+        batch_size: instead of updating an online algorithm after each event, update it after every <batch_event> events
+        average_window: the length of the running average window
+        critical_value: the critical value of the two-tailed-test confidence interval of interest
+
+        :return:
+        """
+
+        # Note, for a two-tailed-test 95% confidence interval:
+        # alpha = 1 - 0.95
+        # critical_value = scipy.stats.norm.ppf(q=(1 - alpha/2))
 
         Arguments = collections.namedtuple(typename='Arguments',
                                            field_names=['slate_size', 'batch_size', 'average_window', 'critical_value'])
         return Arguments(slate_size=5, batch_size=100, average_window=200, critical_value=1.5)
-        
+
     @staticmethod
     def hyperparameters():
 
