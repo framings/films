83aafee36529ace2d5fd5ebb1acd42dc4da3c768
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-10-17 21:37:14 +0100

    Update src/hyperparameters/interface.py

--- src/hyperparameters/interface.py
+++ src/hyperparameters/interface.py
@@ -9,6 +9,19 @@ def main():
 
     logger.info('hyperparameters')
 
+    # the data
+    data = src.data.films.Films().exc()
+    logger.info(f"USERS: {data['userId'].unique().shape}")
+
+    # preprocessing
+    preprocessed = src.data.preprocessing.Preprocessing().exc(data=data, limit=1500)
+    logger.info('\nPreprocessed:\n')
+    logger.info(preprocessed.info())
+
+    # epsilon greedy
+    hyper = src.hyperparameters.epsilongreedy.EpsilonGreedy(data=preprocessed).exc()
+    logger.info(hyper)
+
 
 if __name__ == '__main__':
     # directories/paths
@@ -22,6 +35,8 @@ if __name__ == '__main__':
     logger = logging.getLogger(__name__)
 
     # functions
+    import src.data.films
+    import src.data.preprocessing
     import src.hyperparameters.epsilongreedy
 
     main()
