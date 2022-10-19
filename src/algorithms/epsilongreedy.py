f4a7fb48550c19f2685e88efd25c33bdce6494e0
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-10-19 13:39:55 +0100

    Update src/algorithms/epsilongreedy.py

--- src/algorithms/epsilongreedy.py
+++ src/algorithms/epsilongreedy.py
@@ -36,7 +36,8 @@ class EpsilonGreedy:
         self.logger = logging.getLogger(__name__)
 
         # rewards
-        self.Rewards = collections.namedtuple(typename='Rewards', field_names=['rewards', 'cumulative', 'running'])
+        self.Rewards = collections.namedtuple(typename='Rewards',
+                                              field_names=['rewards', 'cumulative', 'running', 'history'])
 
     def score(self, history: pd.DataFrame, boundary: int, epsilon: float):
         """
@@ -98,19 +99,22 @@ class EpsilonGreedy:
 
         for index in range((self.data.shape[0] // self.batch_size)):
 
-            # temporary break point
+            # Temporary break point
             if index > 1000000:
                 break
 
-            # hence
+            # Hence
             boundary = index * self.batch_size
             history, action_score = self.score(history=history, boundary=boundary, epsilon=epsilon)
+
+            # Note that if action_score is empty then history's input & output lengths will
+            # be the same
             if action_score is not None:
                 values = action_score['liked'].tolist()
                 rewards.extend(values)
 
-        # metrics
+        # Metrics: the rewards series should be calculable via <history>, check
         cumulative = np.cumsum(rewards, dtype='float64')
         running = pd.Series(rewards).rolling(window=self.average_window).mean().iloc[self.average_window:].values
 
-        return self.Rewards(rewards=rewards, cumulative=cumulative, running=running)
+        return self.Rewards(rewards=rewards, cumulative=cumulative, running=running, history=history)
