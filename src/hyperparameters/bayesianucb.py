fd0530ccdcbf987106f232ac6180198c5cb8439b
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-10-20 17:50:47 +0100

    Update src/hyperparameters/bayesianucb.py

---
+++ src/hyperparameters/bayesianucb.py
@@ -0,0 +1,51 @@
+
+import logging
+
+import dask
+import numpy as np
+import pandas as pd
+import scipy.stats
+
+import config
+
+
+class BayesianUCB:
+
+    def __init__(self, data: pd.DataFrame):
+        """
+        dask.distributed.Client(n_workers=1, threads_per_worker=2)
+
+        :param data: The preprocessed modelling data set in focus
+        """
+
+        # the data
+        self.data = data
+
+        # slate_size, batch_size, average_window
+        self.args = config.Config().hyperparameters()
+
+        # Note, for a two-tailed-test 95% confidence interval:
+        # alpha = 1 - 0.95
+        # self.critical_value = scipy.stats.norm.ppf(q=(1 - alpha/2))
+        __alpha = 1 - np.arange(start=0.95, stop=0.99, step=0.01)
+        self.__critical_value = [scipy.stats.norm.ppf(q=(1 - alpha/2)) for alpha in __alpha]
+
+        # logging
+        logging.basicConfig(level=logging.INFO, format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
+                            datefmt='%Y-%m-%d %H:%M:%S')
+        self.logger = logging.getLogger(__name__)
+
+    @dask.delayed
+    def __evaluate(self):
+        """
+
+        :return:
+        """
+
+    def exc(self):
+        """
+
+        :return:
+        """
+
+        self.logger.info(self.__critical_value)
