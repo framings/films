cfbcb5be10c1465d167303c6d8139382042f8947
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-10-20 22:46:23 +0100

    Update src/algorithms/ucb.py

--- src/algorithms/ucb.py
+++ src/algorithms/ucb.py
@@ -5,24 +5,23 @@ import logging
 import numpy as np
 import pandas as pd
 
-import config
-
 
 class UCB:
 
-    def __init__(self, data: pd.DataFrame):
+    def __init__(self, 
+                 data: pd.DataFrame,
+                 args: collections.namedtuple(typename='Arguments',
+                                              field_names=['slate_size', 'batch_size', 'average_window'])):
         """
 
         :param data:
         """
 
         self.data = data
+        self.args = args
+        
+        # arms
         self.arms = self.data['movieId'].unique()
-
-        configurations = config.Config()
-        self.slate_size = configurations.slate_size
-        self.batch_size = configurations.batch_size
-        self.average_window = configurations.average_window
 
         # logging
         logging.basicConfig(level=logging.INFO, format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
@@ -41,20 +40,20 @@ class UCB:
         # the UCB policy applies to the historic dataset prior & equal to the current step
         excerpt = history.loc[history['t'] <= boundary, ]
         if excerpt.shape[0] == 0:
-            recommendations: np.ndarray = np.random.choice(a=self.arms, size=self.slate_size, replace=False)
+            recommendations: np.ndarray = np.random.choice(a=self.arms, size=self.args.slate_size, replace=False)
         else:
             scores = excerpt[['movieId', 'liked']].groupby(by='movieId').agg(mean=('liked', 'mean'), count=('liked', 'count'))
             scores['ucb'] = scores['mean'] + np.sqrt(np.true_divide(2 * np.log10(boundary), scores['count']))
             scores['movieId'] = scores.index
             scores = scores.sort_values('ucb', ascending=False)
-            recommendations: np.ndarray = scores.loc[scores.index[0:self.slate_size], 'movieId'].values
+            recommendations: np.ndarray = scores.loc[scores.index[0:self.args.slate_size], 'movieId'].values
 
         '''
         REPLAY ->
         '''
 
-        # the latest actions set starts from the latest lower boundary, and has self.batch_size records
-        actions = self.data[boundary:(boundary + self.batch_size)]
+        # the latest actions set starts from the latest lower boundary, and has self.args.batch_size records
+        actions = self.data[boundary:(boundary + self.args.batch_size)]
 
         # the intersection of actions & recommendations via `movieId`
         actions = actions.copy().loc[actions['movieId'].isin(recommendations), :]
@@ -81,14 +80,14 @@ class UCB:
         # rewards
         rewards = []
 
-        for index in range((self.data.shape[0] // self.batch_size)):
+        for index in range((self.data.shape[0] // self.args.batch_size)):
 
             # temporary break point
             if index > 9999:
                 break
 
             # hence
-            boundary = index * self.batch_size
+            boundary = index * self.args.batch_size
             history, action_score = self.score(history=history, boundary=boundary)
             if action_score is not None:
                 values = action_score['liked'].tolist()
@@ -96,6 +95,6 @@ class UCB:
 
         # metrics
         cumulative = np.cumsum(rewards, dtype='float64')
-        running = pd.Series(rewards).rolling(window=self.average_window).mean().iloc[self.average_window:].values
+        running = pd.Series(rewards).rolling(window=self.args.average_window).mean().iloc[self.args.average_window:].values
 
         return self.Rewards(rewards=rewards, cumulative=cumulative, running=running)
