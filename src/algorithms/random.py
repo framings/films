efc4ab1ab94bb547d7e7fb09fe6cec14dba40233
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-10-20 22:18:37 +0100

    Update src/algorithms/random.py

--- src/algorithms/random.py
+++ src/algorithms/random.py
@@ -6,28 +6,26 @@ import logging
 import pandas as pd
 import numpy as np
 
-import config
-
 
 class Random:
     """
     Class: Replay
     """
 
-    def __init__(self, data: pd.DataFrame):
+    def __init__(self, 
+                 data: pd.DataFrame,
+                 args: collections.namedtuple(typename='Arguments',
+                                              field_names=['slate_size', 'batch_size', 'average_window'])):
         """
 
         :param data: The modelling data set in focus
         """
 
         self.data = data
-        self.arms = self.data['movieId'].unique()
+        self.args = args
 
-        # configurations
-        configurations = config.Config()
-        self.batch_size = configurations.batch_size
-        self.slate_size = configurations.slate_size
-        self.average_window = configurations.average_window
+        # arms
+        self.arms = self.data['movieId'].unique()
 
         # logging
         logging.basicConfig(level=logging.INFO, format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
@@ -45,15 +43,15 @@ class Random:
         :return:
         """
 
-        # a temporary recommendation function, it recommends self.slate_size films
-        recommendations: np.ndarray = np.random.choice(a=self.arms, size=self.slate_size, replace=False)
+        # a temporary recommendation function, it recommends self.args.slate_size films
+        recommendations: np.ndarray = np.random.choice(a=self.arms, size=self.args.slate_size, replace=False)
 
         '''
         REPLAY ->
         '''
 
-        # the latest actions set starts from the latest lower boundary, and has self.batch_size records
-        actions = self.data[boundary:(boundary + self.batch_size)]
+        # the latest actions set starts from the latest lower boundary, and has self.args.batch_size records
+        actions = self.data[boundary:(boundary + self.args.batch_size)]
 
         # the intersection of actions & recommendations via `movieId`
         actions = actions.copy().loc[actions['movieId'].isin(recommendations), :]
@@ -80,14 +78,14 @@ class Random:
         # rewards
         rewards = []
 
-        for index in range((self.data.shape[0] // self.batch_size)):
+        for index in range((self.data.shape[0] // self.args.batch_size)):
 
             # temporary break point
             if index > 9999:
                 break
 
             # hence
-            boundary = index * self.batch_size
+            boundary = index * self.args.batch_size
             history, action_score = self.score(history=history, boundary=boundary)
             if action_score is not None:
                 values = action_score['liked'].tolist()
@@ -96,7 +94,7 @@ class Random:
         # metrics
         cumulative = np.cumsum(rewards, dtype='float64')
         running = cumulative
-        running[self.average_window:] = running[self.average_window:] - running[:-self.average_window]
-        running = running[self.average_window - 1:] / self.average_window
+        running[self.args.average_window:] = running[self.args.average_window:] - running[:-self.args.average_window]
+        running = running[self.args.average_window - 1:] / self.args.average_window
 
         return self.Rewards(rewards=rewards, cumulative=cumulative, running=running)
