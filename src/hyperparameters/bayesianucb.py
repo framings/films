a00a43c722d920294f8b239c51a3c7c20f8a2c73
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-10-20 19:09:50 +0100

    Update src/hyperparameters/bayesianucb.py

--- src/hyperparameters/bayesianucb.py
+++ src/hyperparameters/bayesianucb.py
@@ -7,6 +7,7 @@ import pandas as pd
 import scipy.stats
 
 import config
+import src.algorithms.bayesianucb
 
 
 class BayesianUCB:
@@ -25,8 +26,9 @@ class BayesianUCB:
         self.args = config.Config().hyperparameters()
 
         # Note, for a two-tailed-test X% confidence interval: alpha = 1 - X/100
-        __alpha = 1 - np.arange(start=0.95, stop=0.99, step=0.01)
-        self.__critical_value = [scipy.stats.norm.ppf(q=(1 - alpha/2)) for alpha in __alpha]
+        self.__alpha = 1 - np.arange(start=0.90, stop=0.97, step=0.01)
+        self.__critical_value = [scipy.stats.norm.ppf(q=(1 - alpha/2)) for alpha in self.__alpha]
+        self.__bayesianucb = src.algorithms.bayesianucb.BayesianUCB(data=self.data, args=self.args)
 
         # logging
         logging.basicConfig(level=logging.INFO, format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
@@ -34,11 +36,15 @@ class BayesianUCB:
         self.logger = logging.getLogger(__name__)
 
     @dask.delayed
-    def __evaluate(self):
+    def __evaluate(self, critical_value: float):
         """
 
         :return:
         """
+
+        scores = self.__bayesianucb.exc(critical_value=critical_value)
+
+        return scores
 
     def exc(self):
         """
@@ -47,7 +53,15 @@ class BayesianUCB:
         """
 
         self.logger.info(f'\nThe number of hyperparameter values: {len(self.__critical_value)}')
+        indices = np.arange(len(self.__critical_value))
 
-        for critical_value in self.__critical_value:
+        computations = []
+        for index in indices:
+            self.logger.info(f'({self.__critical_value[index]}, {self.__alpha[index]})')
+            scores = self.__evaluate(critical_value=self.__critical_value[index])
+            computations.append(scores)
 
-            self.logger.info(critical_value)
+        dask.visualize(computations, filename='bayesianUCB', format='pdf')
+        calculations = dask.compute(computations, scheduler='threads', num_workers=3)[0]
+
+        return calculations
