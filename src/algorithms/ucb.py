ca3b3b6c8d7cf5c1e4e31a6a8a5eb4f46a48501b
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-10-22 20:49:57 +0100

    Update src/algorithms/ucb.py

--- src/algorithms/ucb.py
+++ src/algorithms/ucb.py
@@ -37,7 +37,7 @@ class UCB:
                             datefmt='%Y-%m-%d %H:%M:%S')
         self.logger = logging.getLogger(__name__)
 
-    def score(self, history: pd.DataFrame, boundary: int) -> pd.DataFrame:
+    def score(self, history: pd.DataFrame, boundary: int, scale: float) -> pd.DataFrame:
         """
 
         :return:
@@ -49,7 +49,7 @@ class UCB:
             recommendations: np.ndarray = np.random.choice(a=self.arms, size=self.args.slate_size, replace=False)
         else:
             scores = excerpt[['movieId', 'liked']].groupby(by='movieId').agg(mean=('liked', 'mean'), count=('liked', 'count'))
-            scores['ucb'] = scores['mean'] + np.sqrt(np.true_divide(2 * np.log10(boundary), scores['count']))
+            scores['ucb'] = scores['mean'] + np.sqrt(np.true_divide(scale * np.log10(boundary), scores['count']))
             scores['movieId'] = scores.index
             scores = scores.sort_values('ucb', ascending=False)
             recommendations: np.ndarray = scores.loc[scores.index[0:self.args.slate_size], 'movieId'].values
@@ -62,9 +62,10 @@ class UCB:
 
         return history
 
-    def exc(self) -> pd.DataFrame:
+    def exc(self, scale:float) -> pd.DataFrame:
         """
 
+        :param scale:
         :return:
         """
 
@@ -75,12 +76,12 @@ class UCB:
         for index in range((self.data.shape[0] // self.args.batch_size)):
 
             # temporary break point
-            if index > 100000:
+            if index > 500000:
                 break
 
             # hence
             boundary = index * self.args.batch_size
-            history = self.score(history=history, boundary=boundary)
+            history = self.score(history=history, boundary=boundary, scale=scale)
 
         # in summary
         # ... the raw <rewards> values are the values of field <liked>
