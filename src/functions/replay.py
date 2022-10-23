fd82ae0c9eb6103261d6c6b9193a36ddcdd135a0
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-10-23 22:45:12 +0100

    Update src/functions/replay.py

--- src/functions/replay.py
+++ src/functions/replay.py
@@ -24,7 +24,34 @@ class Replay:
         self.data = data
         self.args = args
 
-    def exc(self, history: pd.DataFrame, boundary: int, recommendations: np.ndarray):
+    def __servings(self, servings: pd.DataFrame,  boundary: int, recommendations: np.ndarray):
+        """
+
+        :param servings:
+        :param boundary:
+        :param recommendations:
+        :return:
+        """
+
+        latest = self.data[boundary:(boundary + self.args.batch_size)]
+        latest = latest.copy()[['userId', 't']].drop_duplicates(ignore_index=True)
+        latest['scoring_round'] = boundary
+
+        suggestions = np.expand_dims(recommendations, axis=0)
+        suggestions_ = pd.DataFrame(data=np.repeat(suggestions, latest.shape[0], axis=0))
+
+        additions = pd.concat([latest, suggestions_], axis=1)
+        additions = additions.melt(id_vars=['userId', 't', 'scoring_round'],
+                                   value_vars=np.arange(suggestions.shape[1]),
+                                   var_name='digit',
+                                   value_name='recommendations')
+        additions.drop(columns='digit', inplace=True)
+
+        servings = pd.concat([servings, additions], axis=0)
+
+        return servings
+
+    def __history(self, history: pd.DataFrame, boundary: int, recommendations: np.ndarray):
         """
 
         :param history:
@@ -46,3 +73,18 @@ class Replay:
         history = pd.concat([history, actions], axis=0)
 
         return history
+
+    def exc(self, history: pd.DataFrame, servings: pd.DataFrame, boundary: int, recommendations: np.ndarray):
+        """
+
+        :param history:
+        :param servings:
+        :param boundary:
+        :param recommendations:
+        :return:
+        """
+
+        servings = self.__servings(servings=servings.copy(), boundary=boundary, recommendations=recommendations)
+        history = self.__history(history=history.copy(), boundary=boundary, recommendations=recommendations)
+
+        return history, servings
