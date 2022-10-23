72b19b50a74104d9aec777575f67d1a922aa23cb
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-10-23 14:54:24 +0100

    Update src/algorithms/epsilongreedy.py

--- src/algorithms/epsilongreedy.py
+++ src/algorithms/epsilongreedy.py
@@ -68,10 +68,7 @@ class EpsilonGreedy:
             scores = scores.sort_values('mean', ascending=False)
             recommendations: np.ndarray = scores.loc[scores.index[0:self.args.slate_size], 'movieId'].values
 
-        '''
-        REPLAY ->
-        '''
-
+        # Evaluation & history update: replay
         history = self.replay.exc(history=history.copy(), boundary=boundary, recommendations=recommendations)
 
         return history
